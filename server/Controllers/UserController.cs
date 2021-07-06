8fef308aa1c40a08db319a3135e6ab12665b8266
Alex <alzombii@gmail.com>
2021-07-06 19:08:28 +0300

    Finished grouping and sorting users

--- server/Controllers/UserController.cs
+++ server/Controllers/UserController.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace server.Controllers
 {
   [ApiController]
   [Route("[controller]")]
-  public class WeatherForecastController : ControllerBase
+  public class UserController : ControllerBase
   {
     [HttpGet]
     public async Task<List<UserGroup>> GetUsers()
--- server/Logic.cs
+++ server/Logic.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Linq;
 
 namespace server
 {
@@ -12,10 +12,28 @@ namespace server
     {
       string filePath = "~/../users.json";
       string text = await File.ReadAllTextAsync(filePath);
-
       List<User> users = JsonConvert.DeserializeObject<List<User>>(text);
+      List<UserGroup> groups = new List<UserGroup>();
 
-      return null; //Task.FromResult<List<UserGroup>>(users);
+      for (int i = 1; i <= 10; i += 3)
+      {
+        int to = i + 2;
+
+        groups.Add(new UserGroup()
+        {
+          From = i,
+          To = to,
+          Users = users.Where(u =>
+          {
+            if (u.Date.Month >= i && u.Date.Month <= to)
+              return true;
+
+            return false;
+          }).OrderBy(u => u.Name.Split().Last()).ToList()
+        });
+      }
+
+      return groups;
     }
   }
 }
